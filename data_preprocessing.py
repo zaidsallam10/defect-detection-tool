d5d90a6a7714e00afe78061358d6f44b5a844b2a
zaidsallam10 <zaidsallam2017@gmail.com>
2022-02-02 20:33:50 +0200

    adding venv

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,12 +1,21 @@
 
 from hashlib import new
 from unicodedata import name
+# sklearn: sikit-learn/learn; its a python library dedicated to call and use machine learning algorithms
+# sklearn: contains:
+# 1. Data preprocessing functions
+# 2. Data Splitting functions (70% for training 15% testing 15% validation)
+# 3. Machine learning algorthims (DT: Decision Tree)
 from sklearn.preprocessing import StandardScaler
+
+
 
 
 
 def checkColumnDataType(col):
     return  col == 'float64'
+
+
 
 
 
@@ -16,13 +25,22 @@ def scaleColumns(df, cols):
     return df
 
 
+
+
+# desctipion; it provides you with the min,max,avg, median, Stander derivation
 def printColDescrption(col):
     print(col.describe())
 
 
 
+
+
+
+
 # this function will loop over columns and scale them
+# it takes the 1. dataset / 2. columns / 3. exported file name
 def scallingValues(df, columns, exported_file_name):
+
 
 
     # Before Scalling
@@ -42,6 +60,7 @@ def scallingValues(df, columns, exported_file_name):
     for col in new_df.columns:
         print("After ;Column Name: ",col)
         print("After; Column Description: ", printColDescrption(new_df[col]) )
+    
     # export new dataset
     new_df.to_csv(exported_file_name, index=False)
 
